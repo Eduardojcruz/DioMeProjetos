ca618f2a82c56d1738c07fcde4dcb8af674931c1
Eduardo José Cruz <eduardo.jose.cruz@outlook.com>
2024-06-01 14:15:01 -0300

    Projetos

--- DesafioLogica/DesafioRankeada.js
+++ DesafioLogica/DesafioRankeada.js
@@ -5,23 +5,26 @@ function calculadoraRankeadas(vitorias, derrotas) {
   if (saldo < 10) {
       rank = 'Ferro';
   } else if (saldo >= 11 && saldo <= 20) {
-      rank = 'Bronze';
+      rank = "Bronze";
   } else if (saldo >= 21 && saldo <= 50) {
-      rank = 'Prata';
+      rank = "Prata";
   } else if (saldo >= 51 && saldo <= 80) {
-      rank = 'Ouro';
+      rank = "Ouro";
   } else if (saldo >= 81 && saldo <= 90) {
-      rank = 'Diamante';
+      rank = "Diamante";
   } else if (saldo >= 91 && saldo <= 100) {
-      rank = 'Lendário';
+      rank = "Lendário";
   } else if (saldo >= 101) {
-      rank = 'Imortal';
+      rank = "Imortal";
+  } else{
+      rank = "Erro"
   }
+  
 
   return rank;
 }
 
-let vitorias = 120;
-let derrotas = 10;
+let vitorias = 300;
+let derrotas = 100;
 let resultado = calculadoraRankeadas(vitorias, derrotas);
-console.log(resultado);
+console.log("Seu rank atual é: " + resultado);
